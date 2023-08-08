cbfd0d51ed6ed0dfaec47f9c11f3fd5466348158
shravan05 <shravan.trainings@gmail.com>
2023-08-07 22:26:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
+// Add comment
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
